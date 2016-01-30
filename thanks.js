b155b5cfe59941ed6a613019407e63cff04ab996
Omer Pines <omerpines@gmail.com>
2016-01-30 22:32:02 +0200

    Initialized Project
    This is what we used in production

---
+++ thanks.js
@@ -0,0 +1,9 @@
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+var email = getParameterByName('email');
+document.getElementsByClassName('email')[0].innerHTML = email;
+document.getElementsByClassName('email')[1].innerHTML = email;
