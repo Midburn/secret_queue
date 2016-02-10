1b0f4cac020e743d5a79dac81626f7cbc061a41d
eyal <eyal.regular@gmail.com>
2016-02-10 19:05:40 +0200

    preset times to send request

--- js/index.js
+++ js/index.js
@@ -1,31 +1,37 @@
-var requestActive = false, executeNow = false;
-var actionUrl = 'https://midburn-queue.herokuapp.com/status';
-var interval = 10000;
+var config = {
+	path: 'https://midburn-queue.herokuapp.com/status',
+	interval: 10000,
+	sendAt: ['19:0:0', '19:0:2', '19:0:4'] // no leading zeros! (e.g. 19:0:0)
+};
 
-function queueStatus() {
-    if (!requestActive) {
-            requestActive = true;
-            $.ajax({
-            url: actionUrl,
-            type: 'post',
-            dataType: 'json',
-            contentType: 'application/json; charset=utf-8'
-        }).done(function(data, status, xhr) {
-            window.location = data.register_page;
-        }).always(function(data, status, xhr) {
-            requestActive = false;
-            $('#last-update').html(new Date());
-            if (executeNow) {
-                executeNow = false;
-                queueStatus();    
-            }
-        });
-    } else { // set to reexecute exactly when request returns
-        executeNow = true;
-    }
-}
+var send = function() {
+	$.ajax({
+		url: config.path,
+		type: 'post',
+		dataType: 'json',
+		contentType: 'application/json; charset=utf-8'
+	}).done(success).fail(error);
+};
 
-$(document).ready(function() {
-    queueStatus();
-    window.setInterval(queueStatus, interval);
-});
+var success = function(data) {
+	window.location = data.register_page;
+};
+
+var error = function(jqXHR, textStatus) {
+	ga('send', 'event', 'index', 'responseError', 'status', textStatus);
+	$('#last-update').html(new Date());
+	setTimeout(send, config.interval);
+};
+
+var checkForTime = function() {
+	var date = new Date();
+	var time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+	if (config.sendAt.indexOf(time) > -1) {
+		send();
+	}
+};
+
+var init = function() {
+	send();
+	setInterval(checkForTime, 1000);
+}();
